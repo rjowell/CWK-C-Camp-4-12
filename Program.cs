978817cc8cfd94bcdddc415ba5d3a4a3cbbc02cf
Russell Jowell <russ.jowell@gmail.com>
2021-04-15 14:27:24 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,302 @@
+﻿using System;
+
+namespace csharp_cwk
+{
+    
+    
+    
+    class Calculator
+    {
+        //Add
+
+        public int DoMath(int numberOne, int numberTwo, char operation)
+        {
+            if(operation == '+')
+            {
+                return numberOne + numberTwo;
+            }
+            else if(operation == '-')
+            {
+                return numberOne - numberTwo;
+            }
+            else if(operation == '*' || operation == 'x')
+            {
+                return numberOne * numberTwo;
+            }
+            else
+            {
+                return -56;
+            }
+        }
+
+
+        public int Add(int numberOne, int numberTwo)
+        {
+            return numberOne + numberTwo;
+        }
+
+        public int Subtract(int numberOne, int numberTwo)
+        {
+            return numberOne - numberTwo;
+        }
+
+        public int Multiply(int numberOne, int numberTwo)
+        {
+            return numberOne * numberTwo;
+        }
+
+        //Subtract
+        //Multiply
+        //Divide
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    class Engine
+    {
+        int horsepower;
+        int engineBlockSize;
+        int gasMileage;
+
+        public Engine()
+        {
+            horsepower = 100;
+            engineBlockSize = 25;
+            gasMileage = 30;
+        }
+    }
+
+
+
+    
+
+
+
+
+    class Car
+    {
+        
+       
+        
+        
+        int wheels = 4;
+        public int gasTankSize = 20;
+        int currentFuel = 7;
+        int wheelSize;
+
+        bool isRunning;
+
+        public void Start()
+        {
+            if(isRunning == true)
+            {
+                Console.WriteLine("This car is already running");
+            }
+            else
+            {
+                isRunning = true;
+            }
+        }
+
+        public void Stop()
+        {
+            if(isRunning == false)
+            {
+                Console.WriteLine("This car is already stopped");
+            }
+            else
+            {
+                isRunning = false;
+            }
+        }
+
+        public void Refuel(int howMuchFuel)
+        {
+            
+            if(currentFuel + howMuchFuel > gasTankSize)
+            {
+                Console.WriteLine("That's too much!");
+            }
+            else
+            {
+                currentFuel += howMuchFuel;
+                //currentFuel = currentFuel + howMuchFuel;
+                Console.Write("Your car was refueled!");
+            }
+        }
+
+        public void drive()
+        {
+            
+            if(isRunning == false)
+            {   
+                Console.WriteLine("You're car isn't started yet!");
+            }
+            else if(currentFuel >= 1)
+            {
+                Console.WriteLine("We're driving down the road!");
+                currentFuel--;
+            }
+            
+            else
+            {
+                Console.WriteLine("You don't have enough fuel. Please add more");
+            }
+            
+            
+        }
+
+        public void speedUp()
+        {
+            Console.WriteLine("Zoom Zoom");
+        }
+
+        public void turn()
+        {
+            Console.WriteLine("turningg!!");
+        }
+
+        int numberOfSeats;
+        string bodyColor;
+        //Red "Blue" "Green" "Pink"
+        string model;
+        float horsepower;
+
+        Engine carsEngine;
+
+        bool isElectric;
+        bool hasGPS;
+        //SUV Limo Truck
+
+        //Constructor Method - Waiter
+        public Car(int wheelSizeInput,string modelTheyWant,string whatColor,int howManySeats, float howMuchHP,bool electric, Engine inputEngine, bool GPS)
+        {
+            wheelSize = wheelSizeInput;
+            model = modelTheyWant;
+            numberOfSeats = howManySeats;
+            bodyColor = whatColor;
+            carsEngine = inputEngine;
+            horsepower = howMuchHP;
+            isElectric = electric;
+            hasGPS = GPS;
+        }
+
+        /*static void Main(string[] args)
+        {
+            /*Engine myEngine = new Engine();
+            
+            Car evansCoolCar = new Car(18,"SUV","Blue",10,420.6f,true,myEngine,true);
+            Console.WriteLine("Evan's car is really cool. It is a "+evansCoolCar.bodyColor+" "+evansCoolCar.model);
+
+            evansCoolCar.Start();
+            evansCoolCar.drive();
+            evansCoolCar.drive();evansCoolCar.drive();evansCoolCar.drive();evansCoolCar.drive();evansCoolCar.drive();evansCoolCar.drive();
+            evansCoolCar.Refuel(11);
+            Console.WriteLine("Your current fuel "+evansCoolCar);
+            evansCoolCar.drive();
+
+            Calculator myCalc = new Calculator();
+
+            Console.WriteLine(myCalc.DoMath(100,20,'%'));
+
+
+            
+            
+            
+            
+            
+          
+            
+        }*/
+
+        
+        
+
+
+    }
+
+    
+}
+
+/**
+
+
+What is a Blueprint?
+
+
+What can you customize on a pickup truck
+Camper / Gem Tops
+Bed Length
+Crew Cab
+Gas/Diesel
+
+
+
+What Do You Need To Build A Car? - Class
+Instructions
+Metal
+Shaped Metal
+Computer Parts
+Screen
+Seats
+Wheels
+Steering Wheel
+Equipment and Tools for Assembly
+Engine
+Engine Coolant
+
+What are some types of cars - Instances / Objects
+Hybrid
+Electric
+Internal Combustion car
+Limos
+Convertibles
+Sedans 
+Coupes
+Vans
+Trucks
+SUVs
+
+How Can You Customize A Car? - Parameters (Adjectives)
+Paint Color
+Interior Materials / Style
+Phone Caddy
+Cup Holders
+GPS
+AC/Heat
+Custom Wheels
+Wheel Size
+MAke/Model
+Gas/Electric
+Hood Ornament
+4WD
+
+What Can A Car Do? - Methods or Functions (verbs)
+Transport People/Cargo
+Cool Down/Heat Up
+Makeshift Generator
+Drive - Forward/Backward
+Turn - LEft/Right
+Speed Up Slow Down
+Do a Stunt
+
+
+*/
---
+++ pizzamachine.cs
@@ -0,0 +1,178 @@
+using System;
+
+namespace pizzamachine
+{
+    
+    class Pizza
+    {
+        public string typeOfPizza;
+        public bool isGlutenFree;
+        public int size;
+
+        public Pizza(string typeInput, bool glutenInput, int sizeIn)
+        {
+            typeOfPizza = typeInput;
+            isGlutenFree = glutenInput;
+            size = sizeIn;
+        }
+
+        //Constructor
+    }
+    
+    
+    class PizzaMaker
+    {
+        public int dough,sauce,cheese,mushrooms;
+        
+        
+        
+        public PizzaMaker(int doughIn,int sauceIn, int cheeseIn, int shroomsIn)
+        {
+            dough = doughIn;
+            sauce = sauceIn;
+            cheese = cheeseIn;
+            mushrooms = shroomsIn;
+        }
+        
+        
+        
+        
+        
+        public Pizza MakePizza(string whatKindOfPizza, bool glutenFree, int whatSize)
+        {
+            
+            
+
+            if(CanIMakeThisPizza(whatKindOfPizza) == true)
+            {
+                
+                if(whatKindOfPizza == "Mushroom")
+                {
+                    dough = dough - 1;
+                    sauce = sauce - 1;
+                    cheese = cheese - 2;
+                    mushrooms = mushrooms - 1;
+
+                    return new Pizza("Mushroom",glutenFree,whatSize);
+                    
+                }
+                else if(whatKindOfPizza == "Crimini")
+                {
+                    dough = dough - 1;
+                    sauce = sauce - 1;
+                    cheese = cheese - 8;
+                    mushrooms = mushrooms - 4;
+                    
+
+                    return new Pizza("Cremini",glutenFree,whatSize);
+                   
+                }
+                
+                else
+                {
+                    return new Pizza("Cheese",false,24);
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            PizzaMaker myMachine = new PizzaMaker(10,6,20,15);
+            Console.WriteLine("Welcome to Mr. Russ Pizza Parlor!");
+            Console.WriteLine("What kind of Pizza do you want?");
+            string orderType = Console.ReadLine();
+            Console.WriteLine("Great choice! Do you want Gluten Free?");
+            string orderGlutenFree = Console.ReadLine();
+            bool glutenStatus;
+            if(orderGlutenFree == "yes")
+            {
+                glutenStatus = true;
+            }
+            else
+            {
+                glutenStatus = false;
+            }
+            Console.WriteLine("Okay, how big do you want it?");
+            int sizeInput =  Int32.Parse(Console.ReadLine());
+
+            if(myMachine.MakePizza(orderType,glutenStatus,sizeInput) != null)
+            {
+                Pizza myPizza = myMachine.MakePizza(orderType,glutenStatus,sizeInput);
+                Console.WriteLine("Congratulations, here is your "+myPizza.typeOfPizza+" and is size:"+myPizza.size);
+            }
+            //Console.WriteLine(myMachine.isThisNumberLessThanTen(4) );
+        }
+        
+        public bool CanIMakeThisPizza(string typeOfPizza)
+        {
+            if(typeOfPizza == "Mushroom")
+            {
+                if(dough < 1 || sauce < 1 || cheese < 2 || mushrooms < 1)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+         
+            }
+            //else if type == cremini
+            //else if type == andrea
+
+            return true;
+        }
+        
+        /*Dough Type/Thickeness
+
+        //
+
+        
+        
+        Dough
+        Sauce
+        Cheese
+        Mushrooms
+        Olive Oil
+        Basil
+        Onions
+        Anchovies
+        
+        
+        FILL THE MACHINE
+        Mushroom - $5.50
+        -1 Dough
+        -1 Sauce
+        -2 Cheese
+        -0.5 mushrooms
+
+        Cremini - $10
+        -1 Dough
+        -8 cheese
+        -4 mushroom
+        -2 Olive Oil
+
+        Andrew Special - $5.25
+        -1 Dough
+        -1 Sauce
+        -2 basil
+        -1 mushroom
+        -3 cheese
+
+        Mr Russ Special - $20.50
+        -1 Dough
+        -1 Sauce
+        -1 Cheese
+        -2 Onions
+        -3 Mushroom
+        -2 Anchovies
+        */
+
+    }
+}
